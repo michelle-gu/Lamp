ecd28c2e283279aca6503f29e4279b08ab9d423b
Michelle Gu <michelle.gu@utexas.edu>
2019-03-06 15:59:22 -0600

    Initial Commit

---
+++ Lamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lamp
+//
+//  Created by Michelle Gu on 3/6/19.
+//  Copyright © 2019 LaMMP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
