41d3f825e1a260293012d45d9a6b278ab89f3fd5
Lindsey Thompson <lindseyatx97@gmail.com>
2019-03-22 14:12:49 -0500

    Fixed sign up navigation screen

--- DerivedData/Lamp/Build/Intermediates.noindex/Lamp.build/Debug-iphonesimulator/Lamp.build/Objects-normal/x86_64/Lamp-Swift.h
+++ DerivedData/Lamp/Build/Intermediates.noindex/Lamp.build/Debug-iphonesimulator/Lamp.build/Objects-normal/x86_64/Lamp-Swift.h
@@ -216,6 +216,7 @@ SWIFT_CLASS("_TtC4Lamp19CardsViewController")
 
 SWIFT_CLASS("_TtC4Lamp31EmailConfirmationViewController")
 @interface EmailConfirmationViewController : UIViewController
+@property (nonatomic, weak) IBOutlet UIButton * _Null_unspecified loginButton;
 - (void)viewDidLoad;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
--- Lamp/Controllers/EmailConfirmationViewController.swift
+++ Lamp/Controllers/EmailConfirmationViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class EmailConfirmationViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
     // MARK: Constants
     let showConfirmedLoginScreen = "showConfirmedLoginScreen"
     
@@ -19,15 +21,6 @@ class EmailConfirmationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
