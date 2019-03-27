447acfb14a46dc7966d6d2782026b302e90aca11
Michelle Gu <michelle.gu@utexas.edu>
2019-03-27 11:27:28 -0500

    Added TableViewCell files for the profile page

---
+++ Lamp/Controllers/LifestylePreferencesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  LifestylePreferencesTableViewCell.swift
+//  Lamp
+//
+//  Created by Michelle Gu on 3/27/19.
+//  Copyright © 2019 LaMMP. All rights reserved.
+//
+
+import UIKit
+
+class LifestylePreferencesTableViewCell: UITableViewCell {
+
+    // MARK: Outlets
+    
+    
+    // MARK: Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
