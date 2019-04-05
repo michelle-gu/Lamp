9b82492166858fc3c30bc59f3976bb57e907ceaa
Michelle Gu <michelle.gu@utexas.edu>
2019-04-05 03:11:05 -0500

    Account deletion working

--- Lamp/Controllers/Settings/AccountViewController.swift
+++ Lamp/Controllers/Settings/AccountViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class AccountViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -17,6 +18,7 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
     let accountSettings = ["Change Password", "Connect with Facebook", "Connect with Google+", "Delete Account"]
     let accountCellIdentifier = "AccountCellIdentifier"
     let changePasswordSegueIdentifier = "changePasswordSegueIdentifier"
+    let unwindToLoginSegueIdentifier = "unwindToLoginSegueIdentifier"
 
     // MARK: Properties
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,8 +40,67 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         if accountSettings[indexPath.row] == "Change Password"{
+        }
+        
+        switch indexPath.row {
+        case 0: // Change password
             let segueIdentifier = "changePasswordSegueIdentifier"
             performSegue(withIdentifier: segueIdentifier, sender: self)
+        case 1:
+            break
+        case 2:
+            break
+        case 3: // Delete account
+            // Prompt the user to re-provide their sign-in credentials
+            let alert = UIAlertController(
+                title: "Delete Account?",
+                message: "Are you sure? This will permanently delete your account. If so, please enter your password.",
+                preferredStyle: .alert)
+            
+            alert.addTextField { (textField) in
+                textField.placeholder = "Password"
+            }
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak alert] (_) in
+                let password = alert!.textFields![0] // Force unwrapping because we know it exists.
+                let user = Auth.auth().currentUser
+                let credential: AuthCredential = EmailAuthProvider.credential(withEmail: (user?.email)!, password: password.text!)
+                user?.reauthenticateAndRetrieveData(with: credential, completion: {(authResult, error) in
+                    if let error = error {
+                        // An error happened.
+                        let alert = UIAlertController(
+                            title: "Password Incorrect",
+                            message: error.localizedDescription,
+                            preferredStyle: .alert)
+    
+                        alert.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(alert, animated: true, completion: nil)
+                    } else {
+                        print("User re-authenticated. Attempting to delete account and return to login.")
+                        user?.delete { error in
+                            if let error = error {
+                                // An error happened.
+                                print("Account deletion failed.")
+                                let alert = UIAlertController(
+                                    title: "Account Deletion Failed",
+                                    message: error.localizedDescription,
+                                    preferredStyle: .alert)
+
+                                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                                self.present(alert, animated: true, completion: nil)
+                                return
+                            } else {
+                                print("Account deleted.")
+                                self.performSegue(withIdentifier: "unwindToLoginSegueIdentifier", sender: self)
+                            }
+                        }
+                    }
+                })
+            }))
+            self.present(alert, animated: true, completion: nil)
+        default:
+            print("Shouldn't get here!")
         }
         
     }
