8279c2427ce783d60a0af2aea38c50574c91e61f
Michelle Gu <michelle.gu@utexas.edu>
2019-04-24 04:29:13 -0500

    Added outlets and minor debugging

--- Lamp/Controllers/Profile/EditProfileTableViewController.swift
+++ Lamp/Controllers/Profile/EditProfileTableViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
 
-class EditProfileTableViewController: UITableViewController {
+class EditProfileTableViewController: UITableViewController, UITextFieldDelegate {
 
     // MARK: - Constants
-//    let user = Auth.auth().currentUser?.uid
-//    let userProfilesRef = Database.database().reference(withPath: "user-profiles")
+    let user = Auth.auth().currentUser?.uid
+    let userProfilesRef = Database.database().reference(withPath: "user-profiles")
     
     // MARK: - Outlets
+    @IBOutlet weak var profilePicView: UIImageView!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var genderField: UITextField!
+    @IBOutlet weak var birthdayField: UITextField!
+    @IBOutlet weak var universityField: UITextField!
+    @IBOutlet weak var futureLocationField: UITextField!
+    @IBOutlet weak var occupationField: UITextField!
+    @IBOutlet weak var bioField: UITextView!
+    @IBOutlet weak var budgetField: UITextField!
+    @IBOutlet weak var numBedroomsField: UITextField!
+    @IBOutlet weak var petsField: UITextField!
+    @IBOutlet weak var smokingField: UITextField!
+    @IBOutlet weak var otherPreferencesField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var facebookField: UITextField!
+    @IBOutlet weak var otherContactField: UITextField!
+    
+    // MARK: - Actions
+    @IBAction func changeProfilePicPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    // MARK: - Functions
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set delegates
+        nameField.delegate = self
 
         // Remove empty cells at bottom
         tableView.tableFooterView = UIView()
@@ -62,60 +102,5 @@ class EditProfileTableViewController: UITableViewController {
             return 0
         }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
