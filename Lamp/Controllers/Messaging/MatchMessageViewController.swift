5dafe2ffa45ece1376b9a13c2752d8f395c150b2
Lindsey Thompson <lindseyatx97@gmail.com>
2019-04-30 09:57:15 -0500

    update user and match profile pictures in match message

--- Lamp/Controllers/Messaging/MatchMessageViewController.swift
+++ Lamp/Controllers/Messaging/MatchMessageViewController.swift
@@ -20,18 +20,59 @@ class MatchMessageViewController: UIViewController {
     
     let user = Auth.auth().currentUser?.uid
     var match = ""
+    
+    // MARK: Properties
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set appropriate Profile pictures to Match Message Card
-        print("NEW MATCH ID = \(match)")
+        ref = Database.database().reference(withPath: "user-profiles")
         
+        // set appropriate Profile pictures to Match Message Card
+        setUserProfilePic()
+        setMatchProfilePic()
         
         styleViewController()
     }
 
     // MARK: Helpers
+    
+    func setUserProfilePic() {
+        let userRef = ref.child(user!)
+        let profile = userRef.child("profile")
+        
+        profile.observe(.value, with: { (snapshot) in
+            let profileDict = snapshot.value as? [String : AnyObject] ?? [:]
+            
+            // set channel profile pic
+            if let profilePicVal = profileDict["profilePicture"] as? String {
+                if profilePicVal != "" {
+                    let profilePicURL = URL(string: profilePicVal)
+                    self.yourProfilePicView.kf.setImage(with: profilePicURL)
+                }
+            }
+        })
+    }
+
+    func setMatchProfilePic() {
+        let matchRef = ref.child(match)
+        let profile = matchRef.child("profile")
+        
+        profile.observe(.value, with: { (snapshot) in
+            let profileDict = snapshot.value as? [String : AnyObject] ?? [:]
+            
+            // set channel profile pic
+            if let profilePicVal = profileDict["profilePicture"] as? String {
+                if profilePicVal != "" {
+                    let profilePicURL = URL(string: profilePicVal)
+                    self.matchProfilePicView.kf.setImage(with: profilePicURL)
+                }
+            }
+        })
+    }
+    
+    // MARK: Actions
     
     @IBAction func keepSwipingButtonClicked(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
